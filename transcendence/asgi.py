219d8a7dc7491eb6f1f8e608fce55d5ea5324504
Stefano Lombardo <slombard@4b0c3.42berlin.de>
2024-05-28 01:00:28 +0200

    wip

--- transcendence/asgi.py
+++ transcendence/asgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
+# your_project/asgi.py
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import pong.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendence.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(pong.routing.websocket_urlpatterns)),
+    }
+)
